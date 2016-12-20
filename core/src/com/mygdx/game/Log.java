43a3135a5bae86d917fd260a26fb3a25f0e34fbb
halll7908 <halll7908@HRH-IC0027341.wrdsb.ca>
2016-12-20 14:25:44 -0500

    Log and Shigeru edits.

--- core/src/com/mygdx/game/Log.java
+++ core/src/com/mygdx/game/Log.java
@@ -4,10 +4,66 @@
  */
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
 /**
  *
  * @author pircn0556
  */
 public class Log {
+    private final float PIPE_GAP = 100;
+    private final float MIN_HEIGHT = 50;
+    private final float MAX_HEIGHT = 350;
+    public static final float WIDTH = 52;
     
+    private Vector2 position;
+    private Texture pipeBottom;
+    
+    private Rectangle bottomBounds;
+    
+    public Log(float x){
+        float y = (int)(Math.random()*(325-75+1) + 75); 
+        position = new Vector2(x,y);
+        log = new Texture("log.png");
+        
+        // replace top with log
+        
+        log = new Rectangle(position.x, position.y - PIPE_GAP/2 - pipeBottom.getHeight(), pipeBottom.getWidth(), pipeBottom.getHeight());
+    }
+    
+    public void render(SpriteBatch batch){
+        batch.draw(pipeTop, position.x, position.y + PIPE_GAP/2);
+        batch.draw(pipeBottom, position.x, position.y - PIPE_GAP/2 - pipeBottom.getHeight());
+    }
+    
+    public float getX(){
+        return position.x;
+    }
+    
+    public void setX(float x){
+        passed = false;
+        position.x = x;
+        float y = (int)(Math.random()*(325-75+1) + 75); 
+        position.y = y;
+        topBounds.setPosition(position.x, position.y + PIPE_GAP/2);
+        bottomBounds.setPosition(position.x, position.y - PIPE_GAP/2 - pipeBottom.getHeight());
+    }
+    
+    public boolean collides(Shigeru b){
+        if(topBounds.overlaps(b.getBounds())){
+            return true;
+        }
+        if(bottomBounds.overlaps(b.getBounds())){
+            return true;
+        }
+        return false;
+    }
+    
+    public void dispose(){
+        pipeTop.dispose();
+        pipeBottom.dispose();
+    }
 }
