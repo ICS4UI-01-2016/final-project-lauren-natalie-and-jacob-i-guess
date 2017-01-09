670effa8ee574e21d55c4007f4cc3d24bdb54ba6
halll7908 <halll7908@HRH-IC0027324.wrdsb.ca>
2017-01-09 13:01:10 -0500

    Enemy work

--- core/src/com/mygdx/game/Enemy.java
+++ core/src/com/mygdx/game/Enemy.java
@@ -21,21 +21,19 @@ public class Enemy {
     private final float MAX_HEIGHT = 350;
     public static final float WIDTH = 52;
     private Vector2 position;
-    private Texture pipeTop;
-    private Texture pipeBottom;
-    private Rectangle topBounds;
-    private Rectangle bottomBounds;
+    private Texture enemy;
+    private Rectangle enemyBounds;
 
     public Enemy(float x) {
         float y = (int) (Math.random() * (325 - 75 + 1) + 75);
         position = new Vector2(x, y);
-        pipeBottom = new Texture("bottomtube.png");
+        enemy = new Texture("zombietemp.png");
 
-        bottomBounds = new Rectangle(position.x, position.y - PIPE_GAP / 2 - pipeBottom.getHeight(), pipeBottom.getWidth(), pipeBottom.getHeight());
+        enemyBounds = new Rectangle(position.x, position.y - PIPE_GAP / 2 - enemy.getHeight(), enemy.getWidth(), enemy.getHeight());
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(pipeBottom, position.x, position.y - PIPE_GAP / 2 - pipeBottom.getHeight());
+        batch.draw(enemy, position.x, position.y - PIPE_GAP / 2 - enemy.getHeight());
     }
 
     public float getX() {
@@ -46,17 +44,17 @@ public class Enemy {
         position.x = x;
         float y = (int) (Math.random() * (325 - 75 + 1) + 75);
         position.y = y;
-        bottomBounds.setPosition(position.x, position.y - PIPE_GAP / 2 - pipeBottom.getHeight());
+        enemyBounds.setPosition(position.x, position.y - PIPE_GAP / 2 - enemy.getHeight());
     }
 
     public boolean collides(Shigeru b) {
-        if (bottomBounds.overlaps(b.getBounds())) {
+        if (enemyBounds.overlaps(b.getBounds())) {
             return true;
         }
         return false;
     }
 
     public void dispose() {
-        pipeBottom.dispose();
+        enemy.dispose();
     }
 }
